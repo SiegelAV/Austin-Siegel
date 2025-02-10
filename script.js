33818b664917aacf0273ded92bcd17393559a1fe
SiegelAV <125154146+SiegelAV@users.noreply.github.com>
2025-02-10 10:52:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const canvas = ReadME.getElementById("canvas1")
+const canvas = README.getElementById("canvas1")
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
